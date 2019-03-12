07abc6a4db50154e826a63398b39959f63715c2e
JeffPatten <jeffspatten@gmail.com>
2019-03-12 15:44:09 -0600

    first commit

---
+++ march12.js
@@ -0,0 +1,14 @@
+// function multiply(a, b){
+//     a * b;
+//   }
+
+// Solution
+function multiply(a, b){
+    let product = a * b;
+    return product
+  }
+
+  let a = 6;
+  let b = 5;
+
+  console.log(product)
